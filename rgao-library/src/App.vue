ff87e442869d81e9ae2700fe64567a9c639c3bd4
Rui Gao <rgao0017@student.monash.edu>
2024-08-12 22:11:38 +1000

    pure css to set user form's info

---
+++ rgao-library/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import LoginForm from './components/LoginForm.vue'
+// import Json from './components/JSON.vue'
+</script>
+
+<template>
+  <!-- <Json /> -->
+  <LoginForm />
+</template>
---
+++ rgao-library/src/components/LoginForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="form">
+    <h1>User Information Form / Credentials</h1>
+    <form action="">
+      <label for="username">Username:</label><br />
+      <input type="text" name="username" id="username" /><br />
+
+      <label for="password">Password:</label><br />
+      <input type="password" name="password" id="password" /><br />
+
+      <label for="isAustralian">isAustralian Resident?</label><br />
+      <input type="checkbox" id="isAustralian" name="isAustralian" /><br />
+
+      <label for="reason">Reason For Joining:</label><br />
+      <textarea name="reason" id="reason" rows="3"></textarea><br />
+
+      <label for="gender">Gender</label><br />
+      <select name="" id="gender">
+        <option value="female">Female</option>
+        <option value="male">Male</option>
+        <option value="other">Other</option>
+      </select>
+    </form>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
