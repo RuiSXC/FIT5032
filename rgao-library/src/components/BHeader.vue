1633608a791c1fba8e4c1e156520f91e17b78671
Rui Gao <rgao0017@student.monash.edu>
2024-09-03 08:57:53 +1000

    initial set-up for Firebase

--- rgao-library/src/components/BHeader.vue
+++ rgao-library/src/components/BHeader.vue
@@ -8,6 +8,10 @@
           <router-link to="/" class="nav-link" active-class="active" aria-current="page">Home (Week 5)</router-link>
         </li>
         <li class="nav-item">
+          <router-link to="/firelogin" class="nav-link" active-class="active"
+            aria-current="page">FireLogin</router-link>
+        </li>
+        <li class="nav-item">
           <router-link to="/about" class="nav-link" active-class="active">About</router-link>
         </li>
         <li class="nav-item">
--- rgao-library/src/router/index.js
+++ rgao-library/src/router/index.js
@@ -1,31 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import LoginView from '@/views/LoginView.vue'
-import AccessDenied from '@/views/AccessDenied.vue'
+// import HomeView from '../views/HomeView.vue'
+// import AboutView from '../views/AboutView.vue'
+// import LoginView from '@/views/LoginView.vue'
+// import AccessDenied from '@/views/AccessDenied.vue'
+import FirebaseSignin from '@/views/FirebaseSignin.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: AboutView,
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: LoginView
-  },
-  {
-    path: '/access-denied',
-    name: 'AccessDenied',
-    component: AccessDenied
+    path: '/firelogin',
+    name: 'FireLogin',
+    component: FirebaseSignin
   }
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: HomeView
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   component: AboutView,
+  //   meta: { requiresAuth: true }
+  // },
+  // {
+  //   path: '/login',
+  //   name: 'Login',
+  //   component: LoginView
+  // },
+  // {
+  //   path: '/access-denied',
+  //   name: 'AccessDenied',
+  //   component: AccessDenied
+  // }
 ]
 
 const router = createRouter({
@@ -34,12 +40,12 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  const isAuthenticated = localStorage.getItem('isAuthenticated');
+  const isAuthenticated = localStorage.getItem('isAuthenticated')
   if (to.meta?.requiresAuth && !isAuthenticated) {
-    next('/access-denied');
+    next('/access-denied')
   } else {
-    next();
+    next()
   }
-});
+})
 
 export default router
