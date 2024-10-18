823e740edbf5a48122333c7435d452e2c5036631
Rui Gao <rgao0017@student.monash.edu>
2024-10-19 00:17:22 +1100

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/FIT5032/'
-    : '/',
+  base: '',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [
     vue(),
     vueDevTools(),
